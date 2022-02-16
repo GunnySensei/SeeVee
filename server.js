b0bc739da25f6ad1bb1d5839eaf8e4ad216105d8
GunnySensei <gunnerwagoner@hotmail.com>
2022-02-15 19:41:57 -0600

    added node packages and gitignore

---
+++ server.js
@@ -0,0 +1,8 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Toggle Tech Blog</title>
+    <link rel="stylesheet" href="/stylesheets/style.css" />
+  </head>
+</html>;
