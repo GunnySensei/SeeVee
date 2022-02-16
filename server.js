d76d328cd572490f2448e4a668baaffa985ab70b
GunnySensei <gunnerwagoner@hotmail.com>
2022-02-15 19:44:41 -0600

    fixed html in js

--- server.js
+++ server.js
@@ -1,8 +1,1 @@
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Toggle Tech Blog</title>
-    <link rel="stylesheet" href="/stylesheets/style.css" />
-  </head>
-</html>;
+
