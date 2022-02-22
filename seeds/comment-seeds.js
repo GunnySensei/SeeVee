62fda9a7de754f42610e888750159202dba01fec
ryan <ryan.williams93@aol.com>
2022-02-21 20:48:13 -0600

    error handled for duplicate comments in comment-seeds

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -14,18 +14,6 @@ const commentData = [
         job_id: null
     },
     {
-        comment_text: "what's your sign, homie?",
-        user_id: 2,
-        code_id: null,
-        job_id: 1
-    },
-    {
-        comment_text: 'better to be safe than sorry',
-        user_id: 1,
-        code_id: 2,
-        job_id: null
-    },
-    {
         comment_text: 'Guess what? Butt!',
         user_id: 2,
         code_id: null,
@@ -37,13 +25,6 @@ const commentData = [
         code_id: 2,
         job_id: null
     },
-    {
-        comment_text: 'FAKE',
-        user_id: 2,
-        code_id: null,
-        job_id: 1
-    },
-
     {
         comment_text: "if u want some real advice, i know a great therapist who also owns a pizzeria. his name's howard and he's a really great doctor! worked wonders on me and my husband with our divorce",
         user_id: 1,
