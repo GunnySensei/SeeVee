327b505484c034493f4eff516a5348934ca09678
GunnySensei <gunnerwagoner@hotmail.com>
2022-02-22 20:12:38 -0600

    links and routing for homepage and subpages

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,3 +1,7 @@
-const router = require('express').Router();
+const router = require("express").Router();
+
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const homeRoutes = require('./home-routes');
-const apiRoutes = require('./api');
-const dashboardRoutes = require('./dashboard-routes');
+const router = require("express").Router();
+const homeRoutes = require("./home-routes");
+const apiRoutes = require("./api");
+const dashboardRoutes = require("./dashboard-routes");
 
-router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
+router.use("/dashboard", dashboardRoutes);
 
 module.exports = router;
