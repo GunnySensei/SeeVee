28703b27de93d86c0af254a33e622a7bacc6daa7
diirtydog <andrew.derek.wales@gmail.com>
2022-02-23 12:28:10 -0600

    undid the server stuff I did for testing purposes

--- server.js
+++ server.js
@@ -1,76 +1,38 @@
-// const path = require("path");
-// const express = require("express");
-// const exphbs = require("express-handlebars");
-// const session = require("express-session");
-// const helpers = require("./utils/helpers");
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const helpers = require("./utils/helpers");
 
-// // Sequelize ORM
-// const sequelize = require("./config/connection");
-// const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
-// const sess = {
-//   secret: "unauthorized credentials",
-//   cookie: { maxAge: 1000 * 60 * 100 },
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize,
-//   }),
-// };
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-// app.use(session(sess));
-
-// const hbs = exphbs.create({ helpers });
-
-// // Handlebars.js template engine
-
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "public")));
-// app.engine("handlebars", hbs.engine);
-// app.set("view engine", "handlebars");
-// app.use(require("./controllers"));
-
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => console.log(`We will eat ass to pass!`));
-// });
-
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const path = require('path');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const helpers = require('./utils/helpers.js');
-const hbs = exphbs.create({ helpers });
+// Sequelize ORM
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-    secret: 'pay them bills hoe',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
+  secret: "unauthorized credentials",
+  cookie: { maxAge: 1000 * 60 * 100 },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+// Handlebars.js template engine
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.use(session(sess));
+app.use(express.static(path.join(__dirname, "public")));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.use(require("./controllers"));
 
-app.use(routes);
-
-sequelize.sync({ force: false }).then (() => {
-    app.listen(PORT, () => console.log('Now listening Cat Daddy!'));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`We will eat ass to pass!`));
 });
-
-// https://awtrekkie.herokuapp.com/
