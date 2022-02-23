6632bbd86c29ef4b68305a9d6ccd4a25cd757ea6
Kevin <kevin.j.wilkerson@gmail.com>
2022-02-22 22:28:31 -0600

    investigating npm start errors

--- controllers/index.js
+++ controllers/index.js
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
 const router = require("express").Router();
-const homeRoutes = require("./home-routes");
 const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes");
 const dashboardRoutes = require("./dashboard-routes");
+const codeChallengeRoutes = require("./code-challenge-routes");
+const jobPostingRoutes = require("./job-post-routes");
+const interviewRoutes = require("./interview-routes");
 
 router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
 router.use("/dashboard", dashboardRoutes);
-=======
-const router = require('express').Router();
-const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes');
-const dashboardRoutes = require('./dashboard-routes');
-const codeChallengeRoutes = require('./code-challenge-routes');
-const jobPostingRoutes = require('./job-post-routes');
-const interviewRoutes = require('./interview-routes')
-
-router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
-router.use('/codes', codeChallengeRoutes);
-router.use('/jobs', jobPostingRoutes);
-router.use('/interviews', interviewRoutes);
->>>>>>> ryDev
+router.use("/codes", codeChallengeRoutes);
+router.use("/jobs", jobPostingRoutes);
+router.use("/interviews", interviewRoutes);
 
 module.exports = router;
