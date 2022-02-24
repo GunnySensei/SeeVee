2cd1d632e3c068ced3a16f2ab3f4292bd60b6a90
Kevin <kevin.j.wilkerson@gmail.com>
2022-02-23 22:45:30 -0600

    final testing of functionality

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
-const helpers = require("./utils/helpers");
 
 // Sequelize ORM
 const sequelize = require("./config/connection");
@@ -21,7 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 // Handlebars.js template engine
 
