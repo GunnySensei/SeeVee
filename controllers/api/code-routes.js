8cde050ca07396abd1d1b03a71ae530ffc2f0043
GunnySensei <gunnerwagoner@hotmail.com>
2022-02-26 14:00:20 -0600

    upvote backend complete

--- controllers/api/code-routes.js
+++ controllers/api/code-routes.js
@@ -1,11 +1,24 @@
 const router = require("express").Router();
 const { Code, User, Comment, Vote } = require("../../models");
+const { sequelize } = require("../../models/user");
 const withAuth = require("../../utils/auth");
 
 // GET all Code route
 router.get("/", (req, res) => {
   console.log("<3<3<3<3<3<3<3<3<3<3<3<3<3<3");
   Code.findAll({
+    attributes: [
+      "id",
+      "title",
+      "code_url",
+      "user_id",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE code.id = vote.code_id)"
+        ),
+        "vote_count",
+      ],
+    ],
     order: [["created_at", "DESC"]],
     include: [
       {
@@ -33,6 +46,18 @@ router.get("/", (req, res) => {
 router.get("/:id", withAuth, (req, res) => {
   Code.findOne({
     where: { id: req.params.id },
+    attributes: [
+      "id",
+      "title",
+      "code_url",
+      "user_id",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE code.id = vote.code_id)"
+        ),
+        "vote_count",
+      ],
+    ],
     order: [["created_at", "DESC"]],
     include: [
       {
@@ -63,27 +88,27 @@ router.get("/:id", withAuth, (req, res) => {
 });
 
 // POST new Code route
-router.post('/', (req, res) => {
-    Code.create({
-        title: req.body.title,
-        code_url: req.body.code_url,
-        user_id: req.session.user_id
-    })
-    .then(dbCodeData => res.json(dbCodeData))
-    .catch(err => {
-        console.log(err);
-        res.status(418).json(err);
+router.post("/", (req, res) => {
+  Code.create({
+    title: req.body.title,
+    code_url: req.body.code_url,
+    user_id: req.session.user_id,
+  })
+    .then((dbCodeData) => res.json(dbCodeData))
+    .catch((err) => {
+      console.log(err);
+      res.status(418).json(err);
     });
 });
 
 // PUT upvote Code route
-router.put('/upvote', withAuth, (req, res) => {
+router.put("/upvote", withAuth, (req, res) => {
   Vote.create({
     user_id: req.session.user_id,
-    code_id: req.body.code_id
+    code_id: req.body.code_id,
   })
-  .then(dbCodeData => res.json(dbCodeData))
-  .catch(err => res.json(err));
+    .then((dbCodeData) => res.json(dbCodeData))
+    .catch((err) => res.json(err));
 });
 
 // PUT update Code route
